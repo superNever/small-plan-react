c7b6d3c43da8e8fe75f005384f7fcc418c1722e8
wanghxm <wanghxm@yonyou.com>
2016-09-14 21:33:24 +0800

    增加一丁点注释

--- src/component/MyInput.js
+++ src/component/MyInput.js
@@ -3,6 +3,7 @@ import '../styles/myInput.less'
 export default class MyInput extends Component{
 	constructor(props){
 	       super(props);
+	       {/** 声明一大推label、input、span的各种状态样式 **/}
 	       this.state = {
 	           changeValue: '',
 	           inputClass:'input-class blur-class',
@@ -14,20 +15,24 @@ export default class MyInput extends Component{
 	           spanShow: 'span-class span-show',
 	           placeholder: this.props.name ? "请输入"+this.props.name :"请输入信息"
 	       };
+	   		{/**input值改变事件**/}
 	       this.changeHandler = (event)=>{
 	       		let obj = {};
 	       		obj.changeValue = event.target.value;
 				this.setState(obj);
 			}
+			{/**input，focus值改变事件**/}
 			this.focusHandler = (event)=>{
 				event.preventDefault();
 				event.target.click();
 				this.setState({waringClass:this.state.spanHide})
 				this.setState({inputClass:this.state.focusClass})
 			}
+			{/**input,blur值改变事件**/}
 			this.blurHandler = (event)=>{
 				event.preventDefault();
 				let boolValue = this.testRegExp(event.target.getAttribute('type'),event.target.value);
+				{/**判断是否匹配规则**/}
 				if(!boolValue){
 					event.target.value = '';
 					this.setState({placeholder:''})
@@ -37,9 +42,11 @@ export default class MyInput extends Component{
 					this.setState({inputClass:this.state.blurClass})
 				}				
 			}
+			{/**点击浮层sapn的事件**/}
 			this.spanClick = (event)=>{
 				event.target.previousSibling.focus();	
 			}
+			{/**正则匹配规则**/}
 			this.testRegExp = (type,value)=>{
 				let boolValue,pattern;
 				switch(type.toLowerCase()){
@@ -59,6 +66,7 @@ export default class MyInput extends Component{
 	}
 	
 	render(){
+		{/**name\placeholder\type解析**/}
 		let name = this.props.name ? this.props.name+":" :"";
 		let placeholder = this.props.name ? "请输入"+this.props.name :"请输入信息";
 		let type = this.props.type ? this.props.type : "text";
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import MyInput from './component/MyInput';
 render(
 	<div>
+		{/** type可以不指定，默认为text，name其实也可以不指定
+		但是不指定样式有点丑，木有调整 **/}
 		<MyInput name="邮箱" type="email"/>
 		<MyInput name="地址" />
 		<MyInput name="电话" type="phone"/>
